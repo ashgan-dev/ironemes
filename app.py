2acf59521719822f768c4fc3f174aebc976385b5
Christophe Le Clerc <chris+framagit@le.clerc.eu.org>
2018-04-10 19:08:36 +0000

    remplacement de l'ORM flask-sqlalchemy pour peewee debut de rendu de pages: ajout de templates et de fichiers statiques

--- app.py
+++ app.py
@@ -3,23 +3,24 @@
 
 # forked from https://github.com/Meewan/MercrediFiction
 
-import time
 from configparser import ConfigParser
 from datetime import datetime
 from multiprocessing.pool import Pool
 from urllib.parse import urlparse
 
+import arrow
 import requests
-from flask import Flask  # , render_template, request, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 from html2text import HTML2Text
 from path import Path
+from peewee import *
 
 ROOT = Path('.').realpath()
 HASHTAGS = ['ironème', 'ironèmes', 'ironeme', 'ironemes']
 BLOCKLIST = ['TrendingBot@mastodon.social', ]
 # API management
 API_URL = 'https://{domain}/api/v1/timelines/tag/{hashtag}?limit={limit}&since_id={since_id}'
+POSTGRESQL_URL = 'postgresql://{user}:{password}@{DB_host}:{port}/{database}'
 
 Path.chdir(ROOT)
 
@@ -32,111 +33,85 @@ config.read(configini)
 app = Flask(__name__)
 # for small configs, use sqlite, with small number of worker (tested with 4 at best on my laptop)
 # big irons, go full blast!
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ironemes.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{user}:{password}@{DB_host}:{port}/{database}'.format(user=config['DB']['user'],
-                                                                                                            password=config['DB']['password'],
-                                                                                                            DB_host=config['DB']['DB_host'],
-                                                                                                            port=config['DB']['port'],
-                                                                                                            database=config['DB']['database'])
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# db = PostgresqlDatabase(config['DB']['database'],
+#                         user=config['DB']['user'],
+#                         password=config['DB']['password'])
+db = SqliteDatabase('app.db')
 
 
-class Toot(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mastodon_id = db.Column(db.BigInteger)
-    creation_date = db.Column(db.DateTime)
-    sensitive = db.Column(db.Boolean)
-    content = db.Column(db.String(4095))
-    favourite_count = db.Column(db.Integer)
-    reblog_count = db.Column(db.Integer)
-    url = db.Column(db.String(4095))
-    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
-    instance_id = db.Column(db.Integer, db.ForeignKey('instance.id'))
-    blacklisted = db.Column(db.Boolean)
+class Instance(Model):
+    """instance db model"""
+    creation_date = DateTimeField()
+    domain = CharField(1024, unique=True)
+    lock = BooleanField()
+    blacklisted = BooleanField()
+
+    class Meta:
+        """related db"""
+        database = db
 
 
-class Account(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mastodon_id = db.Column(db.Integer)
-    username = db.Column(db.String(1024))
-    display_name = db.Column(db.String(4095))
-    creation_date = db.Column(db.DateTime)
-    note = db.Column(db.String(4095))
-    url = db.Column(db.String(4095))
-    avatar = db.Column(db.String(4095))
-    instance_id = db.Column(db.Integer, db.ForeignKey('instance.id'))
-    toots = db.relationship('Toot', backref='account', lazy='dynamic')
-    blacklisted = db.Column(db.Boolean)
+class Account(Model):
+    """account db model"""
+    mastodon_id = IntegerField()
+    username = CharField(1024)
+    display_name = CharField(4095)
+    creation_date = DateTimeField()
+    note = CharField(4095)
+    url = CharField(4095)
+    avatar = CharField(4095)
+    instance = ForeignKeyField(Instance, backref='accounts')
+    blacklisted = BooleanField()
+
+    class Meta:
+        """related db"""
+        database = db
 
 
-class Instance(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    creation_date = db.Column(db.DateTime)
-    domain = db.Column(db.String(1024))
-    toots = db.relationship('Toot', backref='instance', lazy='dynamic')
-    accounts = db.relationship('Account', backref='instance', lazy='dynamic')
-    lock = db.Column(db.Boolean)
-    blacklisted = db.Column(db.Boolean)
+class Toot(Model):
+    """toots db model"""
+    mastodon_id = BigIntegerField()
+    creation_date = DateTimeField()
+    sensitive = BooleanField()
+    content = CharField(4095)
+    favourite_count = IntegerField()
+    reblog_count = IntegerField()
+    url = CharField(4095)
+    account = ForeignKeyField(Account, backref='toots')
+    instance = ForeignKeyField(Instance, backref='toots')
+    blacklisted = BooleanField()
+
+    class Meta:
+        """related db"""
+        database = db
 
 
-class Missed_Link(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(4095))
-    time_misses = db.Column(db.Integer)
+class MissedLink(Model):
+    """missed links db model"""
+    url = CharField(4095, unique=True)
+    time_misses = IntegerField()
+
+    class Meta:
+        """related db"""
+        database = db
 
 
-class Hashtags(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    tag = db.Column(db.String(4095))
-    last_seen_id = db.Column(db.BigInteger)
-    instance_id = db.Column(db.Integer, db.ForeignKey('instance.id'))
+class Hashtags(Model):
+    """Hashtags db model"""
+    tag = CharField(4095)
+    last_seen_id = BigIntegerField(default=0)
+    instance = ForeignKeyField(Instance, backref='hashtags')
+
+    class Meta:
+        """related db"""
+        database = db
 
 
-def save(obj):
+def datetimeformat(value, date_format='DD/MM/YYYY à HH:mm:ss'):
     """
-    commit change to DB
+    formating date for templates
     """
-    db.session.add(obj)
-    db.session.commit()
-
-
-def save_account(instance_name, content):
-    """
-    record account on DB and return id
-    """
-    acct = content['username']
-    if not Account.query.filter_by(username=acct).count():
-        account = Account(mastodon_id=content['id'],
-                          username=content['username'],
-                          display_name=to_text(content['display_name']),
-                          creation_date=datetime.strptime(content['created_at'],
-                                                          '%Y-%m-%dT%H:%M:%S.%fZ'),
-                          note=to_text(content['note']),
-                          url=content['url'],
-                          avatar=content['avatar'],
-                          instance_id=instance_name,
-                          blacklisted=False)
-        save(account)
-    else:
-        account = Account.query.filter_by(username=acct).first()
-    return account.id
-
-
-def add_domain_to_db(domain):
-    """
-    record instance on DB and return id
-    """
-    if not Instance.query.filter_by(domain=domain).count():
-        instance = Instance(creation_date=datetime.now(),
-                            domain=domain,
-                            lock=False,
-                            blacklisted=False)
-        save(instance)
-    else:
-        instance = Instance.query.filter_by(domain=domain).first()
-    return instance.id
+    return arrow.get(value).format(date_format)
 
 
 def to_text(html, rehtml=False):
@@ -164,101 +139,126 @@ def get_hashtags(instance_url):
     fetches all toots for a given hashtag on given instance
     """
     print(instance_url)
+    # last toot ID seen on the current instance
     ref_id = 0
+    # current instance URL
     next_fetch = instance_url
+    # current instance DB details already on DB
     local_instance_url = urlparse(instance_url).netloc
-    instance_details = Instance.query.filter_by(domain=local_instance_url).first()
-    # l'id le plus haut pour l'instance en cours:
-    try:
-        hastag_detail = Hashtags.query.filter_by(instance_id=instance_details.id).first()
-        last_instance_toot_id = hastag_detail.last_seen_id
-    except:
-        last_instance_toot_id = 0
+    instance_details = Instance.get(domain=local_instance_url)
 
+    # hashtags details:
+    # houla, cuila y pique!
+    tag_name = instance_url.split('?')[0].split('/')[-1]
+
+    # did we have already parsed someting?
+    hashtag_detail, _ = Hashtags.get_or_create(instance_id=instance_details.id,
+                                               last_seen_id=0,
+                                               tag=tag_name)
+
+    local_toots = None
     while True:
         try:
             local_toots = requests.get(next_fetch,
                                        timeout=120)
         except:
             # something bad happened, store URL to fetch later
-            missed_link = Missed_Link(url=next_fetch)
-            save(missed_link)
+            MissedLink.get_or_create(url=next_fetch,
+                                     defaults={'time_misses': 0})
+            break
 
         if local_toots:
             try:
                 for toot in local_toots.json():
                     account = toot['account']['acct']
                     instance = urlparse(toot['url']).netloc
-                    if not Instance.query.filter_by(domain=instance).first():
-                        instance_id = add_domain_to_db(instance)
-                    else:
-                        instance_id = Instance.query.filter_by(domain=instance).first().id
+                    # get instance ID or get a new one
+                    instance_id, _ = Instance.get_or_create(domain=instance,
+                                                            defaults={'creation_date': datetime.now(),
+                                                                      'lock': False,
+                                                                      'blacklisted': False})
 
                     if '@' not in account:
                         # we got local account.
                         # so, genuine toots, not federated ones.
                         # let's grab it by the user!
                         account = account + '@' + instance_url
-                        account = save_account(instance_id, toot['account'])
+                        account_saved, _ = Account.get_or_create(mastodon_id=toot['account']['id'],
+                                                        defaults={'username': toot['account']['username'],
+                                                                  'display_name': to_text(toot['account']['display_name']),
+                                                                  'creation_date': datetime.strptime(toot['account']['created_at'],
+                                                                                                   '%Y-%m-%dT%H:%M:%S.%fZ'),
+                                                                  'note': to_text(toot['account']['note']),
+                                                                  'url': toot['account']['url'],
+                                                                  'avatar': toot['account']['avatar'],
+                                                                  'instance_id': instance_id.id,
+                                                                  'blacklisted': False})
 
-                        existing_toot = Toot.query.filter_by(mastodon_id=toot['id'],
-                                                             instance_id=instance_id).first()
-                        if not existing_toot:
-                            db_toot = Toot(mastodon_id=toot['id'],
-                                           creation_date=datetime.strptime(toot['created_at'],
-                                                                           '%Y-%m-%dT%H:%M:%S.%fZ'),
-                                           sensitive=toot['sensitive'],
-                                           account_id=account,
-                                           content=to_text(toot['content'], rehtml=True),
-                                           instance_id=instance_id,
-                                           url=toot['url'],
-                                           favourite_count=toot['favourites_count'],
-                                           reblog_count=toot['reblogs_count'],
-                                           blacklisted=False)
-                            save(db_toot)
+                        toot_saved, _ = Toot.get_or_create(url=toot['url'],
+                                                           defaults={'creation_date': datetime.strptime(toot['created_at'],
+                                                                                                        '%Y-%m-%dT%H:%M:%S.%fZ'),
+                                                                     'sensitive': toot['sensitive'],
+                                                                     'account_id': account_saved.id,
+                                                                     'content': to_text(toot['content'], rehtml=True),
+                                                                     'instance_id': instance_id.id,
+                                                                     'mastodon_id': toot['id'],
+                                                                     'favourite_count': toot['favourites_count'],
+                                                                     'reblog_count': toot['reblogs_count'],
+                                                                     'blacklisted': False})
+
                 next_fetch = local_toots.links['next']['url']
                 id_to_fetch = int(next_fetch.split('=')[-1])
                 if id_to_fetch > ref_id:
                     ref_id = id_to_fetch
 
-                if last_instance_toot_id > id_to_fetch:
+                if hashtag_detail.last_seen_id > id_to_fetch:
                     break
 
-                time.sleep(1)  # avoid hitting limit rates
+                # time.sleep(1)  # avoid hitting limit rates
             except (KeyError, ):
                 # no more dicts, no scraping left
                 # oui did it \o/
                 break
         else:
-            missed_link = Missed_Link(url=next_fetch)
-            save(missed_link)
-    # houla, cuila y pique!
-    tag_name = instance_url.split('?')[0].split('/')[-1]
-    hastag_detail = Hashtags.query.filter_by(instance_id=instance_details.id).first()
-    try:
-        hastag_detail.tag = tag_name
-        hastag_detail.last_seen_id = ref_id
-        save(hastag_detail)
-    except:
-        hastag_detail = Hashtags(tag=tag_name,
-                                 last_seen_id=ref_id,
-                                 instance_id=instance_details.id)
-        save(hastag_detail)
+            missed_link = MissedLink.get_or_create(url=next_fetch,
+                                                   defaults={'time_misses': 0})
+
+    query = Hashtags.update(last_seen_id=ref_id).where((Hashtags.instance_id == instance_details.id) &
+                                                       (Hashtags.tag == hastag_detail.tag))
+    query.execute()
 
 
-db.create_all()
+app.add_template_filter(datetimeformat)
 
-if not Instance.query.filter_by(domain=config['Auth']['instance']).first():
-    add_domain_to_db(urlparse(config['Auth']['instance']).netloc)
+
+@app.route('/')
+def start_page():
+    """main page"""
+    return render_template('template.tpl',
+                           toots=Toot.select().order_by(Toot.creation_date))
+
+
 if __name__ == '__main__':
+    db.connect()
+    db.create_tables([Instance, Account, Toot, MissedLink, Hashtags])
+
+    domain = urlparse(config['Auth']['instance']).netloc
+    query = Instance.get_or_create(domain=domain,
+                                   defaults={'creation_date': datetime.now(),
+                                             'lock': False,
+                                             'blacklisted': False})
+
     to_fetch = list()
     for k in HASHTAGS:
-        for i in [x.domain for x in Instance.query.all()]:
+        for i in [x.domain for x in Instance.select()]:
             to_fetch.append(API_URL.format(domain=i,
                                            hashtag=k,
                                            since_id=0,
                                            limit=40))
-    with Pool(8) as p:
+    # we're *not really* CPU/mem/HDD bound. so yepekai!
+    # for info, with 32 workers, on average:
+    # <2Mbits/s download, 25-50% CPU, 1.5/2Go Ram, <10% HDD use on my core I7-4710 laptop
+    with Pool(4) as p:
         result = p.map(get_hashtags, to_fetch)
 
-    # app.run()
+    app.run(debug=True)
